d9192847fb73915791ccdd37f111927350902f06
andreagonz <andreagonz@ciencias.unam.mx>
2018-08-02 22:41:25 -0500

    asdnaosd

--- buscador.py
+++ buscador.py
@@ -24,7 +24,23 @@ class Buscador():
 
     def busqueda(self, s):
         return None
+    
+    def formato(lst_res, tipo='txt'):
+        if tipo == 'xml':
+            return res_xml(lst_res)
+        if tipo == 'html':
+            return res_html(lst_res)
+        return res_txt(lst_res)
 
+    def res_xml(lst_res):
+        return xml
+
+    def res_html(lst_res):
+        return html
+
+    def res_txt(lst_res):
+        return txt
+        
 class BuscadorGoogle(Buscador):
 
     def busqueda(self, s):
