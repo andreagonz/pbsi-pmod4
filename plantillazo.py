a9217b4baeb4baec3d2f83df02885f63d98ed582
andreagonz <andreagonz@ciencias.unam.mx>
2018-08-03 15:44:29 -0500

    ajsdb

--- plantillazo.py
+++ plantillazo.py
@@ -1,14 +1,6 @@
-
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #UNAM-CERT
-
-###TOR
-
-"""
-***EJEMPLO***
-python req.py -p 9889 -s  87.118.110.170 -U romina.martinez -P P4ssw0rd123 -T
-"""
 
 """
 Se debe instalar TOR
@@ -18,77 +10,85 @@ De igual forma se debe instalar socks
 Debian: apt-get install python-socks
 """
 
-###USER-AGENT
-
-"""
-En este caso se quiso modificar el User-Agent personalizado, pero de igual forma se pueden usar los mas comunes 
-o incluso hacerlo de forma aleatoria en:
-https://pypi.python.org/pypi/fake-useragent
-"""
-
 import sys
 import optparse
-from requests import get
-from requests.exceptions import ConnectionError
-from urllib import urlopen
-import socks
-import socket
-
+import navegacion as nav
+from formato import formato
 
 def printError(msg, exit = False):
-        """
-        Imprime mensaje de Error y sale del programa
-        """
-        sys.stderr.write('Error:\t%s\n' % msg)
-        if exit:
-            sys.exit(1)
+    """
+    Imprime mensaje de Error y sale del programa
+    """
+    sys.stderr.write('Error:\t%s\n' % msg)
+    if exit:
+        sys.exit(1)
 
 def addOptions():
-
     """
     Aquí se configuraron las banderas posibles para la ejecución del programa
     """
-
     parser = optparse.OptionParser()
-    parser.add_option('-v','--verbose', dest='verbose', default=None, action='store_true', help='If specified, prints detailed information during execution.')
-    parser.add_option('-p','--port', dest='port', default='80', help='Port that the HTTP server is listening to.')
-    parser.add_option('-T', '--tor', dest='tor', default=None, action="store_true", help='Make request with TOR')
-    opts,args = parser.parse_args()
-    return opts
-    
-def checkOptions(options):
-        """
-        Verifica que se haya ingresado una dirección de servidor
-        """
-    if options.server is None:
-        printError('Debes especificar un servidor a atacar.', True)
-
-def torito(options):
-        """
-        Al ingresar la bandera -T, utiliza servidores TOR para realizar el request
-        """ 
-    if options.tor != None:
-        print '\n \t Se usará TOR \n'
-        socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9050)
-        socket.socket = socks.socksocket
-    else:
-        pass
+    parser.add_option('-v', '--verbose', dest='verbose', default=None, action='store_true', help='If specified, prints detailed information during execution.')
+    parser.add_option('-n', '--num-res', dest='num_res', default='50', help='Port that the HTTP server is listening to.')
+    parser.add_option('-b', '--buscadores', dest='buscadores', default='google', help='Port that the HTTP server is listening to.')
+    parser.add_option('-N', '--no-params', dest='no_params', default=False, action='store_true', help='If specified, prints detailed information during execution.')
+    parser.add_option('-r', '--regex', dest='regex', default=False, action="store_true", help='Make request with TOR')
+    parser.add_option('-d', '--domains', dest='domains', default=False, action="store_true", help='Make request with TOR')
+    parser.add_option('-u', '--user-agents', dest='user_agents', default=None, help='Port that the HTTP server is listening to.')
+    parser.add_option('-p', '--proxies', dest='proxies', default=None, help='Port that the HTTP server is listening to.')
+    parser.add_option('-f', '--formato', dest='formato', default='txt', help='Port that the HTTP server is listening to.')
+    return parser.parse_args()
 
 def myip():
-        """
-        Devuelve la IP pública con la que se está realizando el request
-        """
+    """
+    Devuelve la IP pública con la que se está realizando el request
+    """
     my_ip = urlopen('http://ip.42.pl/raw').read()
-    print 'La IP que se eśtá usando es: ', my_ip
+    print('La IP que se eśtá usando es: ', my_ip)
 
+def lee_proxies(proxies):
+    with open(proxies) as f:
+        for p in f.readlines():
+            tmp = {}
+            l = [x.strip().split('=') for x in p.split(',')]
+            for x in l:
+                if x[0] == 'http':
+                    tmp['http'] = x[1]
+                elif x[0] == 'https':
+                    tmp['https'] = x[1]
+            if len(tmp) > 0:
+                nav.proxies.append(tmp)
 
+def lee_user_agents(user_agents):
+    with open(user_agents) as f:
+        for u in f.readlines():
+            nav.user_agents.append(u.strip())
 
+def int_or_0(n):
+    try:
+        return int(n)
+    except:
+        return 0
+    
 if __name__ == '__main__':
     try:
-        opts = addOptions()
-        checkOptions(opts)
-        torito(opts) 
-        myip()
+        opts, args = addOptions()
+        if len(args) == 0:
+            print("Uso: python3 %s <busqueda> [parametros]" % sys.argv[0])
+            sys.exit(1)
+        query = args[0]
+        if opts.proxies:
+            lee_proxies(opts.proxies)
+        if opts.user_agents:
+            lee_user_agents(opts.user_agents)
+        buscadores = []
+        fabrica = FabricaBuscador()
+        for x in opts.buscadores.split(','):
+            buscadores.append(fabrica.get_buscador(x.strip()))
+        resultados = []
+        for b in buscadores:
+            resultados += b.busqueda(query, int_or_0(opts.num_res), opts.no_params, opts.regex)
+        print(formato(resultados, opts.formato, opts.domains))
+        # print(nav.hacer_peticion("http://google.com").text)
     except Exception as e:
-        printError('Ocurrio un error inesperado')
-printError(e, True)
+        printError('Ocurrio un error inesperado: %s' % str(e))
