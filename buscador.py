63bd3218d0e91a124401986eb3d5092d1d0078db
andreagonz <andreagonz@ciencias.unam.mx>
2018-08-02 22:47:41 -0500

    asdnaosd

--- buscador.py
+++ buscador.py
@@ -22,54 +22,54 @@ class Resultado():
 
 class Buscador():
 
-    def busqueda(self, s):
+    def busqueda(self, s, max_res=50, no_params=False):
         return None
     
-    def formato(lst_res, tipo='txt'):
+    def formato(lst_res, tipo='txt', domains=False):
         if tipo == 'xml':
-            return res_xml(lst_res)
+            return res_xml(lst_res, domains)
         if tipo == 'html':
-            return res_html(lst_res)
-        return res_txt(lst_res)
+            return res_html(lst_res, domains)
+        return res_txt(lst_res, domains)
 
-    def res_xml(lst_res):
+    def res_xml(lst_res, domains=False):
         return xml
 
-    def res_html(lst_res):
+    def res_html(lst_res, domains=False):
         return html
 
-    def res_txt(lst_res):
+    def res_txt(lst_res, domains=False):
         return txt
         
 class BuscadorGoogle(Buscador):
 
-    def busqueda(self, s):
+    def busqueda(self, s, max_res=50, no_params=False):
         return []
     
 class BuscadorBing(Buscador):
 
-    def busqueda(self, s):
+    def busqueda(self, s, max_res=50, no_params=False):
         return []
     
 class BuscadorDuckduckgo(Buscador):
 
-    def busqueda(self, s):
+    def busqueda(self, s, max_res=50, no_params=False):
         
         return []
 
 class BuscadorPastebin(Buscador):
 
-    def busqueda(self, s):
+    def busqueda(self, s, max_res=50, no_params=False):
         return []
     
 class BuscadorBoardreader(Buscador):
 
-    def busqueda(self, s):
+    def busqueda(self, s, max_res=50, no_params=False):
         return []
     
 class BuscadorZoneH(Buscador):
 
-    def busqueda(self, s):
+    def busqueda(self, s, max_res=50, no_params=False):
         return []
     
 
